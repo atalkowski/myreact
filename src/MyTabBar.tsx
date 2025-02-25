8527e6d0270e2c5e2f52d80d0862ea178e261ba5
Andy Talkowski <atalkowski@ea.com>
2025-02-24 22:14:49 -0800

    Removed dead link to original notes

--- src/MyTabBar.tsx
+++ src/MyTabBar.tsx
@@ -8,12 +8,9 @@ const MyTabBar = () => {
   const [activeTab, setActiveTab] = useState('entrees');  
   const handleTabClick = (tabName) => {
     setActiveTab(tabName);
-    navigate(tabName === "original" ? "/notes/index.html" : tabName); 
+    navigate(tabName); 
   };
 
-  const openNewTab = (url) => {
-    window.open(url, '_blank');
-  };
 
   return (
     <div>
@@ -22,7 +19,6 @@ const MyTabBar = () => {
         <button className={activeTab === 'entrees' ? 'active' : ''} onClick={() => handleTabClick('entrees')}>Links</button>&nbsp;&nbsp;
         <button className={activeTab === 'groups' ? 'active' : ''} onClick={() => handleTabClick('groups')}>Groups</button>&nbsp;&nbsp;
         <button className={activeTab === 'jobs' ? 'active' : ''} onClick={() => handleTabClick('jobs')}>Jobs</button>&nbsp;&nbsp;
-        <button className={activeTab === 'original' ? 'active' : ''} onClick={() => openNewTab('/notes/index.html')}>Original ... </button>
         
       </div>
       <div className="tab-content">
@@ -32,7 +28,6 @@ const MyTabBar = () => {
         {activeTab === 'entrees' && <div><b>Links:</b> as your aide memoires - put into groups</div>}
         {activeTab === 'groups'  && <div><b>Groups:</b> to set up categories for your links</div>}
         {activeTab === 'jobs'    && <div><b>Jobs:</b> to track your job applications (we don't need this!)</div>}
-        {activeTab === 'original'  && <div><b>Original:</b> what this demo is trying to replace</div>}
         </p>
       </div>
 
